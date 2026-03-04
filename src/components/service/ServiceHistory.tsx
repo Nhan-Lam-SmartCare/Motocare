7b974cb799826f5c14069f9ebc61a73d9b3132ec
Nhan-Lam-SmartCare <nhan04344@gmail.com>
2026-03-04 16:47:07 +0700

    init

--- src/components/service/ServiceHistory.tsx
+++ src/components/service/ServiceHistory.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect, useCallback } from "react";
+﻿import React, { useState, useMemo, useEffect, useCallback } from "react";
 import { useAppContext } from "../../contexts/AppContext";
 import {
   formatCurrency,
@@ -1212,7 +1212,7 @@ export const ServiceHistory: React.FC<ServiceHistoryProps> = ({
                 >
                   <path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z" />
                 </svg>
-                <span>{storeSettings?.phone || "0947.747.907"}</span>
+                <span>{storeSettings?.phone || "0907.239.337"}</span>
               </div>
               {storeSettings?.email && (
                 <div

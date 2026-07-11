c8fada5614ad1203cb6868ba9d2f4b5db902049f
Nhan-Lam-SmartCare <nhan04344@gmail.com>
2026-07-11 15:57:43 +0700

    init

--- extract_components.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const file = path.join('g:', 'Motocare', 'src', 'components', 'service', 'WorkOrderMobileModal.tsx');
-let content = fs.readFileSync(file, 'utf8');
-
-// We will find boundaries using search strings.
-// But it's better to just do this with simple start/end find.
-
